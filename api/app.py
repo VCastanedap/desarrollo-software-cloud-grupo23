fd76756a6eb8b79478d0fb13297c1c0363926d54
Victor Castaneda <v.castanedap@uniandes.edu.co>
2023-10-27 18:07:07 -0500

    Clean app

--- api/app.py
+++ api/app.py
@@ -14,31 +14,28 @@ app.secret_key = "your_secret_key"
 
 jwt = JWTManager(app)
 
+
+upload_folder = app.config["UPLOAD_FOLDER"]
+
+
 @app.route('upload/<filename>')
 def upload_file():
-    from broker.tasks import tasks
+    pass
 
-
-
-    tasks('nombre_archivo')
-    return response()
 
 @app.route("/download/<filename>")
 def download_file(filename):
     try:
-        upload_folder = app.config["UPLOAD_FOLDER"]
         file_path = os.path.join(upload_folder, filename)
-
-        # Logeamos los permisos del archivo
+        
         file_stat = os.stat(file_path)
         permissions = stat.filemode(file_stat.st_mode)
         app.logger.info(f"Permissions for {file_path}: {permissions}")
 
         app.logger.info(
-            f'Contenido del directorio "uploads" antes de la descarga: {os.listdir(upload_folder)}'
+            f'Uploads before download: {os.listdir(upload_folder)}'
         )
 
-        # Utilizando send_file con ruta absoluta
         absolute_path = os.path.abspath(file_path)
         return send_file(absolute_path, as_attachment=True)
     except FileNotFoundError:
@@ -48,10 +45,6 @@ def download_file(filename):
         return "Archivo no encontrado", 404
 
 
-
-
-
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
